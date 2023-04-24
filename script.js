ecc2c88663f124a161eed158da5fa1dcce113e9c
divyaprakashjava <131693337+divyaprakashjava@users.noreply.github.com>
2023-04-24 22:01:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var js = "one"
+if (js == "two") { window.alert("This is tow") }
+else { window.alert("this is one") }
+document.write(js)
+console.log(js);
+document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
+let firstName = "Divya";
+let lastName = "Prakash";
+document.write(firstName + "_" + lastName);
